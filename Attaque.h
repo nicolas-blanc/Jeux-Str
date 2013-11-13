24ae30f62bf9dd0475249f1a5a66f437adff0557
Nicolas <nicolas.r.blanc@gmail.com>
2013-11-13 13:12:19 +0100

    test 1

---
+++ Attaque.h
@@ -0,0 +1,20 @@
+#ifndef ATTAQUE_H
+#define	ATTAQUE_H
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Attaque {
+public:
+    Attaque(float degat = 0.0 , int portee = 0 , int pointaction = 0);
+    void Attaquer();// a completer ...
+
+private:
+    float m_Degat;
+    int m_Portee; //comment ca va marcher ????? est ce un cercle ou ..
+    int m_PointAction;
+};
+
+
+#endif	/* ATTAQUE_H */
+
