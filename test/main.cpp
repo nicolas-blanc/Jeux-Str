cefc1a5d98be92f02c4eb3f10bf06261fa237576
bahrines <bahrines@pc-dg-033-04.iut2.upmf-grenoble.fr>
2013-11-13 15:03:49 +0100

    1er commit

---
+++ test/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: bahrines
+ *
+ * Created on 13 novembre 2013, 15:02
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
