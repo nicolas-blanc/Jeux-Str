319695d6bd0b307949d1b3d7d6cf1495768e03be
Nakiva <william_94@hotmail.fr>
2013-11-19 19:34:42 +0100

    Adaptation à la classe Entite

--- Case.cpp
+++ Case.cpp
@@ -1,28 +1,24 @@
 #include "Case.h"
 #include "Effet.h"
 
-template <class T>
-Case<T>::Case(int x, int y) : CaseGen(x,y) {}
 
-template <class T>
-void Case<T>::setOccupant(T occ) {
+Case::Case(int x, int y) : CaseGen(x,y) {}
+
+void Case::setOccupant(Entite occ) {
 	occupant = occ;
 }
 
-template <class T>
-void Case<T>::transmettreAttaque(int nbPV) {
+void Case::transmettreAttaque(int nbPV) {
     if (occupant!=NULL) {
             occupant.modifPV(nbPV);
     }
 }
 
-template <class T>
 bool Case<T>::isOccupee() {
     return (occupant != NULL);
 }
 
-template <class T>
-void Case<T>::declencherEffets() {
+void Case::declencherEffets() {
     int vectSize = effets.size();
     for (int i=0; i<vectSize; i++) {
         effets[i].lancerSort(this);
