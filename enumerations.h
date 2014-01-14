355a321f337c84f479c18be7327c2c2d1a2e5e31
nicolas-blanc <nicolas.r.blanc@gmail.com>
2014-01-15 00:14:41 +0100

    Ajout enumeration.h

---
+++ enumerations.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   enumerations.h
+ * Author: HolyPony
+ *
+ * Created on 13 janvier 2014, 12:51
+ */
+
+#ifndef ENUMERATIONS_H
+#define	ENUMERATIONS_H
+
+enum catBatiments {chateau, tour};
+enum catUnite {guerrier, chevalier, voleur, magicien, pretre, archer};
+
+#endif	/* ENUMERATIONS_H */
+
